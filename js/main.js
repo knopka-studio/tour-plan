95617094836d1b83060b8f4798628921f0ce6489
knopka_studio <tararinalexandra@gmail.com>
2021-07-26 01:15:30 +0600

    added: slider section

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
