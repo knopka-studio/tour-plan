e5faf0a5f1751856066d68c7127c03d1dc03b3dc
knopka_studio <tararinalexandra@gmail.com>
2021-07-28 15:23:12 +0600

    added: reviews section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-const swiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel__slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider__button-next',
-    prevEl: '.slider__button-prev',
+    nextEl: '.hotel-slider__button-next',
+    prevEl: '.hotel-slider__button-prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button-next',
+    prevEl: '.reviews-slider__button-prev',
   },
 
   keyboard: {
